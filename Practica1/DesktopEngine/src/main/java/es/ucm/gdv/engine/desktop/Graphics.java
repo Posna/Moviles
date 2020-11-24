d48284da9c192325ccc7a7ca8fbfdc56aaadd865
DavidCarmonaFauste <davicarm@ucm.es>
2020-11-24 17:53:09 +0100

    debería pintar

--- Practica1/DesktopEngine/src/main/java/es/ucm/gdv/engine/desktop/Graphics.java
+++ Practica1/DesktopEngine/src/main/java/es/ucm/gdv/engine/desktop/Graphics.java
@@ -7,14 +7,24 @@ import java.awt.Paint;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
+import javax.swing.JFrame;
+
 import es.ucm.gdv.engine.Font;
 
 public class Graphics implements es.ucm.gdv.engine.Graphics {
 
     Graphics2D graphics_;
+    JFrame paint;
 
-    public void init(Graphics2D g){
-        graphics_  = g;
+    float translateX_; //Transformacion
+    float translateY_;
+    float scale_; //Escalado
+    float angle;
+
+
+    public void init(java.awt.Graphics g){
+        graphics_  = (Graphics2D) g;
+        paint= new JFrame();
     }
     /**
      * Pinta una linea dadas dos posiciones
@@ -24,7 +34,11 @@ public class Graphics implements es.ucm.gdv.engine.Graphics {
      * @param y2 Y del segundo punto
      */
     public void drawLine(float x1, float y1, float x2, float y2){
-
+        graphics_.drawLine((int)x1, (int)y1, (int)x2, (int)y2);
+    }
+    public void setColor(int r, int g, int b, int a){
+        Color myColor = new Color(r, g, b);
+        graphics_.setColor(myColor);
     }
 
     /**
@@ -35,7 +49,7 @@ public class Graphics implements es.ucm.gdv.engine.Graphics {
      * @return Devuelve una fuente
      */
     public Font newFont(String filename, int size, boolean isBold){
-
+            return null;
     }
 
     /**
@@ -45,7 +59,8 @@ public class Graphics implements es.ucm.gdv.engine.Graphics {
      * @param b
      */
     public void clear(int r, int g, int b){
-        //graphics_.fil;
+        setColor(r,g,b, 255);
+        fillRect(0,0,getWidth(),getHeight());
     }
 
 
@@ -74,21 +89,38 @@ public class Graphics implements es.ucm.gdv.engine.Graphics {
      */
     public void drawText(String text, float x, float y){
         graphics_.drawString(text, x, y);
-        //graphics_.
     }
 
     /**
      * @return Devuelve el ancho de la pantalla
      */
     public int getWidth(){
-        return graphics_.getWidth();
+        return paint.getWidth();
     }
-
     /**
      * @return Devuelve el alto de la pantalla
      */
     public int getHeight(){
-        return  graphics_.getHeight();
+        return  paint.getHeight();
+    }
+
+    public void translate(float x, float y){
+        graphics_.translate(x, y);
+        translateX_ = x; translateY_ = y;
+    }
+
+    public void scale(float x){
+        graphics_.scale(-x, x);
+        scale_  = x;
+    }
+
+    public void rotate(float angle){
+        graphics_.rotate(angle);
+    }
+
+    //Disposes of this graphics context and releases any system resources that it is using.
+    public void dispose(){
+        graphics_.dispose();
     }
 
 }
